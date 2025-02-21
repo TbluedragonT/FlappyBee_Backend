9b3a3e80b43268720439879cb126854e26160237
Dave T <51966691+TbluedragonT@users.noreply.github.com>
2025-02-20 22:34:34 -0500

    Update Deploy_EatieNFT.js

--- scripts/Deploy_EatieNFT.js
+++ scripts/Deploy_EatieNFT.js
@@ -4,13 +4,13 @@ async function deployContract() {
   const ExampleNFT = await hre.ethers.getContractFactory("EatieNFT");
   const exampleNFT = await ExampleNFT.deploy();
   await exampleNFT.deployed();
-  // This solves the bug in Mumbai network where the contract address is not the real one
+  // This solves the bug in Mumbai network where the contract address is not the real one.
   const txHash = exampleNFT.deployTransaction.hash;
   const txReceipt = await ethers.provider.waitForTransaction(txHash);
   const contractAddress = txReceipt.contractAddress;
   console.log("EatieNFT Contract deployed to address:", contractAddress);
 }
-
+//deploy
 deployContract()
   .then(() => process.exit(0))
   .catch((error) => {
